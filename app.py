c7f0106e387a352d25b99c105335c3e6b2a1a0e0
KatePham <thuy.pham@notabasement.com>
2020-12-04 19:08:45 +0700

    add logging to debug app.py

--- app.py
+++ app.py
@@ -7,16 +7,16 @@ import os
 
 def create_app():
     flask_app = Flask(__name__)
-
+    print("flask_app")
     # flask app configs
     flask_app.config.from_object('config.Config')
-
+    print("flask_app.config")
     # init db
     migrate = Migrate()
     db.init_app(flask_app)
-
+    print("db.init_app(flask_app)")
     migrate.init_app(flask_app, db, "migrations")
-
+    print("migrate.init_app(flask_app, db, migration)")
     # register blueprints
     flask_app.register_blueprint(terminal)
 
@@ -25,8 +25,8 @@ def create_app():
 
 app = create_app()
 
-with app.app_context():
-    upgrade(directory="migrations")  # run db upgrade
+# with app.app_context():
+#     upgrade(directory="migrations")  # run db upgrade
 
 if __name__ == "__main__":
     app.run(debug=True)
