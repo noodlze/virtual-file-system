7de25b45829ae102842a169be9fd3365d62296d0
KatePham <thuy.pham@notabasement.com>
2020-12-07 13:34:20 +0700

    revert config to deploy to heroku

--- config.py
+++ config.py
@@ -11,13 +11,14 @@ class Config:
     """Base config."""
     APP_NAME = 'virtual-file-system'
     TESTING = False
-    SECRET_KEY = os.environ.get('SECRET_KEY', None)
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'vnVl8ycSmt6b6veHk4KP')
     SESSION_TYPE = 'sqlalchemy'
     SESSION_SQLALCHEMY_TABLE = 'sessions'
 
     # seems like heroku cannot load env file -> include engine uri here though bad pracice
     SQLALCHEMY_DATABASE_URI = os.environ.get(
-        'ENGINE_URI', None)
+        'ENGINE_URI', 'postgres://qfaqxzdlxtirlz:0397be39cfcaf0bbd34053427baf3b64ab1d08c56fc3dab5c7835bf59514b10a@ec2-3-231-48-230.compute-1.amazonaws.com:5432/dbke29vrr6jfru?sslmode=require')
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     SESSION_COOKIE_NAME = 'session'
