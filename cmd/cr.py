7a1ccf9c23046cb02705deb0e93506dfc85f6b4b
KatePham <thuy.pham@notabasement.com>
2020-12-12 13:47:17 +0700

    improve cr command deal with create in file edge case

--- cmd/cr.py
+++ cmd/cr.py
@@ -3,6 +3,7 @@ import os
 import re
 import argparse
 from utils.response import InvalidCmdError
+from models.item import Item
 from utils.paths import to_abs_path
 from utils.db_session import provide_db_session
 from cmd.db_helpers import item_exists, splitall, add_new_item, to_abs_path_ids
@@ -10,7 +11,8 @@ from collections import namedtuple
 import sys
 
 # CR [-p] PATH [DATA]
-CrArgs = namedtuple("CrArgs", ["requires_create", "parent_id", "path", "data"])
+CrArgs = namedtuple(
+    "CrArgs", ["requires_create", "data", "path_parts", "path_parts_id"])
 FILE_NAME_RE = r'^[a-zA-Z0-9 _-]+$'
 
 
@@ -39,24 +41,31 @@ def check_cr_cmd_args(cmd_args, db=None):
 
         path = parsed_args.path[0]
         abs_path = to_abs_path(path)
+
         # cr path arg cannot contain *
         if '*' in abs_path:
             raise InvalidCmdError(
                 message=f'cr: PATH cannot contain *: {" ".join(cmd_args)}')
+
+        # validate item name
         validate_item_name(os.path.basename(abs_path))
 
         data = parsed_args.data
 
         # check if abs_path exists
-        # dest_path_parts, dest_path_parts_id = to_abs_path_ids(abs_path=abs_path)
-        i_exists, _ = item_exists(abs_path=abs_path)
-        print(f'does item {abs_path} exist={i_exists}')
-        if i_exists:
+        path_parts, path_parts_id = to_abs_path_ids(abs_path=abs_path)
+
+        for _item_id in path_parts_id[:-1]:
+            # item exists and item is a file
+            if _item_id != -1 and not Item.is_a_dir(_item_id):
+                raise InvalidCmdError(
+                    message=f'cr: {abs_path}: not a directory')
+
+        if path_parts_id[-1] != -1:
             raise InvalidCmdError(message=f'cr: {abs_path}: File exists')
 
         # if item does not exist, does the parent dir exist?
-        parent_exists, parent_id = item_exists(
-            abs_path=os.path.dirname(abs_path))
+        parent_exists = True if path_parts_id[-2] != -1 else False
 
         # parent dir does not exist + unable to create parent dir
         if not parent_exists and not allow_create:
@@ -66,7 +75,7 @@ def check_cr_cmd_args(cmd_args, db=None):
         requires_create = True if (
             allow_create and not parent_exists) else False
 
-        return CrArgs(requires_create, parent_id, abs_path, data)
+        return CrArgs(requires_create, data, path_parts, path_parts_id)
 
     except SystemExit as e:
         # (str(e) == "0") -> --help/-h
@@ -78,35 +87,29 @@ def check_cr_cmd_args(cmd_args, db=None):
 
 @provide_db_session
 def execute_cr_cmd(cr_args, db=None):
-    print("execute cr cmd", cr_args.path)
-    print("PARENT_ID={},path={},data={}".format(
-        cr_args.parent_id, cr_args.path, cr_args.data))
-    all_parts = splitall(cr_args.path)
-    item_name = all_parts[-1]
-    print("cr -p allparts={}".format(all_parts))
+    abs_path = '/'.join(cr_args.path_parts)
+    print("execute cr cmd", "{}".format(abs_path))
     # identify parent id of path to create
-    parent_id = BASE_DIR_ID
-    if cr_args.requires_create:
-        # TODO: optimize
-        parent_id = BASE_DIR_ID
-        _path_itr = ""
-        for p in all_parts[:-1]:  # create everything in parent directories
-            print("Checking item {}".format(p))
-            _path_itr += p
-            exists, item_id = item_exists(
-                abs_path=_path_itr)
-            if exists:  # already exists
-                parent_id = item_id
-            else:  # add new item
-                parent_id = add_new_item(
-                    name=p, item_parent_id=parent_id)  # create a directory
-    else:
-        parent_id = cr_args.parent_id
+    parent_id = cr_args.path_parts_id[-2]
 
-    # add new item to the parent id
+    if cr_args.requires_create:
+        parent_id = cr_args.path_parts_id[0]
+        # create second -> last directory
+        for _part_name, _part_id in zip(cr_args.path_parts[1:-1], cr_args.path_parts_id[1:-1]):
+            if _part_id == -1:  # does not exist
+                # create a directory
+                created_item_id = add_new_item(
+                    name=_part_name, item_parent_id=parent_id)
+                # update parent_id
+                parent_id = created_item_id
+            else:
+                parent_id = _part_id
+
+        # add new item to the parent id
     print("cr_args.data=", cr_args.data)
-    add_new_item(name=item_name, item_parent_id=parent_id, data=cr_args.data)
+    add_new_item(name=cr_args.path_parts[-1],
+                 item_parent_id=parent_id, data=cr_args.data)
     resp = {
-        "response": "Created {}".format(cr_args.path)
+        "response": "Created {}".format(abs_path)
     }
     return resp
